a241267d8974ee78e2ccda56b4cc4832dd1ac4ff
Chris Cahill <christopherfishcahill@gmail.com>
2022-12-30 09:22:13 -0500

    fix while loop bug

--- R/om_hcr.R
+++ R/om_hcr.R
@@ -143,7 +143,7 @@ get_fit <- function(hcrmode = c(
   if (hcrmode != "dfo") {
     opt <- nlminb(obj$par, obj$fn, obj$gr, upper = upper, lower = lower)
     ctr <- 1
-    if (opt$convergence == 1 && ctr < 5) {
+    while (opt$convergence == 1 && ctr < 5) {
       tmb_pars <- list(par = opt$par)
       obj <- MakeADFun(tmb_data, tmb_pars, silent = T, DLL = "om_hcr")
       opt <- nlminb(obj$par, obj$fn, obj$gr, upper = upper, lower = lower)
@@ -271,16 +271,16 @@ system.time({
 
 utility <- dat %>%
   group_by(
-    hcr, obj, convergence,
+    hcr, obj, convergence, pdHess,
     criterion
   ) %>%
   summarise(
     obj = unique(obj) / max(dat$obj), hcr = unique(hcr),
-    convergence = unique(convergence),
+    convergence = unique(convergence), pdHess = unique(pdHess),
     criterion = unique(criterion)
   ) %>%
   arrange(desc(obj))
-
+utility
 # now for yield
 system.time({
   dat <- NULL
@@ -299,12 +299,12 @@ system.time({
 
 yield <- dat %>%
   group_by(
-    hcr, obj, convergence,
+    hcr, obj, convergence, pdHess,
     criterion
   ) %>%
   summarise(
     obj = unique(obj) / max(dat$obj), hcr = unique(hcr),
-    convergence = unique(convergence),
+    convergence = unique(convergence), pdHess = unique(pdHess),
     criterion = unique(criterion)
   ) %>%
   arrange(desc(obj))
@@ -313,9 +313,9 @@ utility
 yield
 
 ################################################################################
-# extra plotting junk
+# testing
 sim_dat <- get_recmult(pbig = pbig, Rbig, sdr)
-opt <- get_fit(hcrmode = "logit-linear", objmode = "yield")
+opt <- get_fit(hcrmode = "rect", objmode = "yield")
 opt[[1]]$obj[1]
 opt[[2]]
 
@@ -328,6 +328,141 @@ unique(opt[[1]]$pdHess)
 # For yield:
 # spline, rect, db logistic, exp, logit, 
 # logit-linear* (bumps against bound every time)
+
+
+
+# Debugging junk: 
+# objmode = "yield"
+# hcrmode = "rect"
+# 
+# tmb_data <- list(
+#   n_year = length(years),
+#   n_age = length(ages),
+#   vbk = vbk,
+#   s = s,
+#   cr = cr,
+#   rinit = rinit,
+#   ro = ro,
+#   uo = uo,
+#   asl = asl,
+#   ahv = ahv,
+#   ahm = ahm,
+#   upow = case_when(
+#     objmode == "yield" ~ 1,
+#     objmode == "utility" ~ upow
+#   ),
+#   ages = ages,
+#   recmult = sim_dat$dat$recmult,
+#   hcrmode = case_when(
+#     hcrmode == "OM" ~ 0,
+#     hcrmode == "linear" ~ 1,
+#     hcrmode == "logistic" ~ 2,
+#     hcrmode == "spline" ~ 3,
+#     hcrmode == "rect" ~ 4,
+#     hcrmode == "db_logistic" ~ 5,
+#     hcrmode == "exponential" ~ 6,
+#     hcrmode == "logit" ~ 7,
+#     hcrmode == "logit-linear" ~ 8,
+#     hcrmode == "dfo" ~ 9
+#   ),
+#   knots = c(0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.5, 2.0, 10),
+#   dfopar = c(Umsy, Bmsy),
+#   vmult = exp(sd_survey * rnorm(length(years)) - 0.5 * (sd_survey)^2)
+# )
+# if (hcrmode == "OM") {
+#   tmb_pars <- list(par = rep(0.1, length(years)))
+# } else if (hcrmode == "linear") {
+#   tmb_pars <- list(par = c(0.1, 0.1))
+# } else if (hcrmode == "logistic") {
+#   tmb_pars <- list(par = rep(0.1, 3))
+# } else if (hcrmode == "spline") {
+#   tmb_pars <- list(par = rep(0.1, length(tmb_data$knots)))
+# } else if (hcrmode == "rect") {
+#   tmb_pars <- list(par = c(0.02, 0.01, 0.1))
+#   if (objmode == "yield") {
+#     tmb_pars <- list(par = c(0.9, 0.01, 0.9))
+#   }
+# } else if (hcrmode == "db_logistic") {
+#   tmb_pars <- list(par = c(0.2, rep(0.5, 4)))
+#   # tmb_pars <- list(par = c(0.3, 10, 0.7, 10, 0.6))
+# } else if (hcrmode == "exponential") {
+#   tmb_pars <- list(par = rep(0.1, 3))
+# } else if (hcrmode == "dfo") {
+#   tmb_pars <- list(par = rep(0.1, 3)) # just a filler for dfo policy
+# } else if (hcrmode == "logit") {
+#   tmb_pars <- list(par = rep(0, 3))
+# } else if (hcrmode == "logit-linear") {
+#   tmb_pars <- list(par = c(0.6, 1, 0.7))
+# }
+# 
+# if (hcrmode == "OM") {
+#   lower <- rep(0, length(years))
+#   upper <- rep(1, length(years))
+# }
+# if (hcrmode != "OM") {
+#   lower <- rep(-Inf, length(tmb_pars$par))
+#   upper <- rep(Inf, length(tmb_pars$par))
+# }
+# if (hcrmode == "spline") {
+#   lower <- rep(0, length(tmb_pars$par))
+#   upper <- rep(Inf, length(tmb_pars$par))
+# }
+# if (hcrmode == "db_logistic") {
+#   lower <- c(-Inf, 0, -Inf, 0, -Inf)
+#   upper <- c(1, Inf, Inf, Inf, Inf)
+# }
+# if (hcrmode == "logit-linear") {
+#   lower <- c(-Inf, -Inf, -Inf)
+#   upper <- c(Inf, 100, Inf)
+# }
+# if (!"om_hcr" %in% names(getLoadedDLLs())) {
+#   dyn.load(TMB::dynlib("src/om_hcr"))
+# }
+# obj <- MakeADFun(tmb_data, tmb_pars, silent = F, DLL = "om_hcr")
+# opt <- nlminb(obj$par, obj$fn, obj$gr, upper = upper, lower = lower)
+# opt$convergence
+# opt$objective
+# ctr <- 1
+#   
+# while (opt$convergence == 1 && ctr < 5) {
+#     tmb_pars <- list(par = opt$par)
+#     obj <- MakeADFun(tmb_data, tmb_pars, silent = T, DLL = "om_hcr")
+#     opt <- nlminb(obj$par, obj$fn, obj$gr, upper = upper, lower = lower)
+#     ctr <- ctr + 1
+# }
+# opt$objective
+#   
+# 
+# pdHess <- sdreport(obj)$pdHess
+#   objective <- -opt$objective
+#   convergence <- opt$convergence
+#   pdHess <- ifelse(pdHess == TRUE, 0, 1)
+# }
+# 
+# 
+# 
+# 
+# 
+# 
+# ctr <- 1
+#   if (opt$convergence == 1 && ctr < 5) {
+#     tmb_pars <- list(par = opt$par)
+#     obj <- MakeADFun(tmb_data, tmb_pars, silent = T, DLL = "om_hcr")
+#     opt <- nlminb(obj$par, obj$fn, obj$gr, upper = upper, lower = lower)
+#     ctr <- ctr + 1
+#   }
+#   pdHess <- sdreport(obj)$pdHess
+#   objective <- -opt$objective
+#   convergence <- opt$convergence
+#   pdHess <- ifelse(pdHess == TRUE, 0, 1)
+# }
+# 
+# 
+
+
+
+
+
 
 
 plot(opt[[1]]$Ut ~ opt[[1]]$Vulb,
